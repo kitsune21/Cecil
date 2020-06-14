c9a41a14c5f197323fdfe79726ee50130ecad04b
kitsune21 <kitsunesama21@gmail.com>
2020-06-13 20:28:42 -0600

    started working on home page

--- src/Home/HomeContent.js
+++ src/Home/HomeContent.js
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
+import {Container, Card, ListGroup, Image} from 'react-bootstrap';
+import bass from './Images/bass.jpg';
 
 class HomeContent extends Component {
 
   render() {
     return(
-      <div>
-        <div id="home">
-          <p>Hey! I'm Cecil!</p>
-          <p>I am a Full Stack Web Developer</p>
-          <p>I love JavaScript!</p>
-          <p>I am a big fan of React and Node.</p>
-          <p>Check out my random thoughts about movies.</p>
-          <p>And if you are looking to hire people, check out my Resume!</p>
-        </div>
-      </div>
+      <Container fluid>
+          <Card>
+            <Card.Title>Social</Card.Title>
+            <Card.Body>
+              <ListGroup>
+                <ListGroup.Item>Twitter</ListGroup.Item>
+                <ListGroup.Item>Discord</ListGroup.Item>
+                <ListGroup.Item>Twitch</ListGroup.Item>
+                <ListGroup.Item>GitHub</ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Title>Picture</Card.Title>
+            <Card.Body>
+              <Image src={bass}/>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Title>About Me</Card.Title>
+          </Card>
+        </Container>
     )
   }
 }
