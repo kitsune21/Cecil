be86b528e3b9c56b62e3f7b91e3bed991babaff3
Cecil Thomas <me@cecil-thomas.com>
2021-06-14 17:46:31 -0600

    removed the minecraft tab

--- src/Home/Menu.js
+++ src/Home/Menu.js
@@ -10,7 +10,7 @@ class MenuBar extends Component {
         <ul id='menu'>
           <li id='menuTitle'><Link to='/'>Cecil Thomas</Link></li>
           <li id='menuItem'><Link to='/movies'>Movie Reviews</Link></li>
-          <li id='menuItem'><Link to='/minecraft'>Minecraft Server</Link></li>
+          {/* <li id='menuItem'><Link to='/minecraft'>Minecraft Server</Link></li> */}
           <li id='menuItem'><Link to='/resume'>Resume</Link></li>
           <li id='menuItem'><a href='https://toolring.cecil-thomas.com' rel='external' target='__blank'>ToolRing</a></li>
           {/* <li id='menuItem'><a href='/bucketlist'>Bucket List</a></li> */}
